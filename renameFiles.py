a8dfdfae7d00a0f797c8a48a1fd8df4a30f05304
Moaaz Elzayat <tmoaaz@gmail.com>
2017-10-25 09:26:54 +0200

    Rename a sort of files in directory using python

---
+++ renameFiles.py
@@ -0,0 +1,12 @@
+import os
+import string
+def rename_files():
+    keep_files = os.listdir(r"C:\Users\moaaz\Desktop\prank")
+    print (keep_files)
+    table = str.maketrans(dict.fromkeys('0123456789'))
+    os.chdir(r"C:\Users\moaaz\Desktop\prank")
+    print(os.getcwd())
+    for file_name in keep_files:
+        os.rename(file_name,file_name.translate(table))
+
+rename_files()
